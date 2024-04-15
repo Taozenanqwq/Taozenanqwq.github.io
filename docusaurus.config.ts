16f4497e91b2c45a0dfb970f39f46d821846236e
taozenan <tzn1104@126.com>
2024-04-15 17:04:23 +0800

    feat: 部署个人站点

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
   tagline: 'Nothing is impossible to a willing heart',
   favicon: 'img/favicon.ico',
   // Set the production url of your site here
-  url: 'https://Taozenanqwq.github.io/Tao-blog',
+  url: 'https://Taozenanqwq.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
